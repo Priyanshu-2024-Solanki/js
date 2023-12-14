6e745aac2583dff2670c2ae88a372d2f2a30b787
Priyanshu-2024-Solanki <153825113+Priyanshu-2024-Solanki@users.noreply.github.com>
2023-12-14 15:47:35 +0000

    conersion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+let score = "33abc";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+
+let scre = null;
+
+console.log(typeof scre);
+console.log(typeof (scre));
+
+let value_InNumber = Number(scre);
+console.log(typeof (value_InNumber));
+console.log(value_InNumber);
+
+// if undefined -> then after conversion it gives NaN(not a Numeber)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1 , false => 0
+
+// convert to boolean 
+
+// 1 => true , 0 => false 
+// "" => false
+// "hitesh" => true
+
