93ae29bd87f969d8346293137153dac85f72f974
Priyanshu-2024-Solanki <153825113+Priyanshu-2024-Solanki@users.noreply.github.com>
2023-12-14 15:26:38 +0000

    change

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 14453
+let accountEmail = "nakul@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+console.table([accountId , accountEmail , accountCity , accountPassword , accountState])
+
+/*
+   prefer not to use var 
+   due to issue in block and functional scope
+*/
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs , not browser
+
+// dataTypes 
+
+// number => 2^53
+// bigInt
+// string = ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique 
+
+// object 
+
+console.log(null); // object 
+console.log(undefined); // undefined 
+
