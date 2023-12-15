209de92e3a7bdd44c89d4020de3786ff139949e6
Priyanshu Solanki <153825113+Priyanshu-2024-Solanki@users.noreply.github.com>
2023-12-15 00:41:59 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,20 +1,20 @@
 let score = "33abc";
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(typeof (valueInNumber));
-console.log(valueInNumber);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
 
 let scre = null;
 
-console.log(typeof scre);
-console.log(typeof (scre));
+// console.log(typeof scre);
+// console.log(typeof (scre));
 
 let value_InNumber = Number(scre);
-console.log(typeof (value_InNumber));
-console.log(value_InNumber);
+// console.log(typeof (value_InNumber));
+// console.log(value_InNumber);
 
 // if undefined -> then after conversion it gives NaN(not a Numeber)
 
@@ -27,4 +27,36 @@ console.log(value_InNumber);
 // 1 => true , 0 => false 
 // "" => false
 // "hitesh" => true
+
+// *************operations***************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "hello" , str2 = " nakul";
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1" + 2); // string 
+// console.log(1 + "2"); // string 
+// console.log("1" + "2"); // string 
+// console.log(1 + 2); // number 
+
+// some tricky ones 
+
+// console.log("1" + 2 + 2); // 122 
+// console.log(1 + 2 + "3"); // 33 
+
+// try not to depend output of such messy lines
+
+
 
