b3150b41e24895a5beab1b2773e0c83084f76fde
Priyanshu Solanki <153825113+Priyanshu-2024-Solanki@users.noreply.github.com>
2023-12-15 00:56:51 +0000

    compare

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 <= 1);
+// etc return boolean value(true or false)
+
+// console.log("2" >= 1); // true 
+// console.log("2" > 1); // true
+
+// console.log(null > 0); // false 
+// console.log(null == 0); // false 
+// console.log(null >= 0); // true 
+// (>= and >) converts null to 0 while equality(==) works slightly diffrently in JS
+
+// console.log(undefined >0); // false 
+// console.log(undefined == 0); // false 
+// console.log(undefined >= 0); // false
+
+// avoid such type of comparison which can 
+// lead to unexpected results 
+
+// === 
+
+// console.log("2" == 2); // true
+// console.log("2" === 2); // false 
