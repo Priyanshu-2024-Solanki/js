8aecee95ba9a70f28732d231169e1032ad31ebbf
Priyanshu Solanki <153825113+Priyanshu-2024-Solanki@users.noreply.github.com>
2023-12-17 02:24:51 +0000

    new

---
+++ 05_iterations/two.js
@@ -0,0 +1,16 @@
+// while 
+
+// let index = 0;
+// while(index <= 10)
+// {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2;
+// }
+
+// do while 
+
+let index = 11;
+
+do {
+    console.log(`Value of index is ${index}`);
+}while(index <= 10)
