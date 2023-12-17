6a49bff9d18cd945e2992e99eea28c69f94ede05
Priyanshu Solanki <153825113+Priyanshu-2024-Solanki@users.noreply.github.com>
2023-12-17 05:58:15 +0000

    map and chaianing

---
+++ 05_iterations/seven.js
@@ -0,0 +1,11 @@
+const myNumbers = [1,2,3,4,5,6,7,8,9];
+
+// const newNum = myNumbers.map( (num)=> {return num+10});
+
+// chaining 
+// const newNum = myNumbers
+//                    .map((item) => item*10)
+//                    .map((item) => item+1)
+//                    .filter((item) => item>=40)
+
+console.log(newNum);
